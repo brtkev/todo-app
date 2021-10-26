024491b4cc3fcd4f3c25d0b0b3e7697495a5afc3
kev <kbreto2911@gmail.com>
2021-10-26 16:10:45 -0400

    task show/hide description

--- src/components/task.js
+++ src/components/task.js
@@ -1,46 +1,53 @@
 import styles from '../styles/task.module.css'
 import { useEffect, useState } from 'react';
 
+let clearDescriptionsWasAdded = false;
+const clearDescriptions = (ev) => {
+    if(ev.target.closest(`[dropdown-description-button]`)) return;
+    if(!ev.target.closest(`.${styles.description}`)) closeDescriptionsExcept();
+}
 
-export default function Task() {
-
-    const [descriptionHeight, setDescriptionHeight] = useState(0);
-    useEffect(() => {
-        const description = document.getElementsByClassName(styles.container)[0]
-        setDescriptionHeight(description.offsetHeight)
-        description.style.height = window.innerWidth * 0.05 + "px";
+export default function Task({index}) {
+    useEffect(()=> {
+        if(!clearDescriptionsWasAdded){
+            document.addEventListener('click', clearDescriptions)
+            clearDescriptionsWasAdded = true;
+        };
     }, [])
 
-    const downClick = (ev) => {
-        const description = ev.target.parentNode.parentNode.parentNode;
-        if (description.offsetHeight === descriptionHeight){
-            description.style.height = window.innerWidth * 0.05 + "px";
-        }else{
-            description.style.height = descriptionHeight + 'px';
-        }
+    const toggleDescription = (ev) => {
+        const currentDescription = ev.target.closest(`.${styles.container}`).children[1];
+        currentDescription.classList.toggle(styles.show);
+        closeDescriptionsExcept(currentDescription);
     }
 
   return(
     <div className={styles.container}>
-        <div className={styles.titleBox} >
+        <div className={styles.titleBox} style={{zIndex: index}} >
             <h3 className={styles.title} >title</h3>
             
             <div>
                 <TaskButton>ch</TaskButton>
                 <TaskButton>x</TaskButton>
-                <TaskButton onClick={downClick} >d</TaskButton>
+                <TaskButton onClick={toggleDescription} dropdown-description-button="true" >d</TaskButton>
             </div>
         </div>
         
-        <div className={styles.description} >
+        <div className={styles.description} style={{zIndex: index-1}} >
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sapiente, debitis accusantium molestias dicta harum. Incidunt expedita at, cupiditate corporis magnam dolore reprehenderit delectus consectetur quis, porro, itaque nesciunt laudantium?
         </div>
     </div>
     );
 };
 
-const TaskButton = (props) => {
+export function closeDescriptionsExcept(currentDescription = undefined){
+    const descriptionsList = document.querySelectorAll(`.${styles.description}`);
+    descriptionsList.forEach( description => {
+        if(description !== currentDescription) description.classList.remove(styles.show);
+    });
+}
 
+const TaskButton = (props) => {
     return(
         <button className={styles.button} {...props} >{props.children}</button>
     );
--- src/components/todo.js
+++ src/components/todo.js
@@ -1,10 +1,15 @@
 import styles from '../styles/todo.module.css';
 import Task from './task';
 export default function Todo(){
-
+    const arr = [];
+    for(let i = 6; i >=  0; i= i - 2){
+        if(i > 0){
+            arr.push(<Task key={"taskCard" + i} index={i} />);
+        }
+    }
     return(
         <div className={styles.container}>
-           <Task /> 
+            {arr}
         </div>
     )
 }
