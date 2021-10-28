5b40aaa9de457d820fa58210c198e6d212dcac9c
kev <kbreto2911@gmail.com>
2021-10-28 12:47:16 -0400

    title with plus button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import styles from './styles/App.module.css'
 import useDocTitle from './hooks/useDocTitle';
 import Todo from './components/todo';
+import Title from './components/title';
 
 export default function App(){
 	useDocTitle("todo app");
 
 	return (
 		<div className={styles.container} >
-			
+			<Title />
 			<Todo />
 		</div>
 	);
--- src/components/images.js
+++ src/components/images.js
@@ -1,9 +1,11 @@
 import downArrow from '../../public/down-arrow.png';
 import cancel from '../../public/close.png';
 import pencil from '../../public/pencil.png';
+import plus from '../../public/plus.png'
 
 export {
     downArrow,
     cancel,
-    pencil
+    pencil,
+    plus
 }
